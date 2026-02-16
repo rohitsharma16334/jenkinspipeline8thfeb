0118f21c75f8f7dc9033a2931b61d5d41a6274d6
rohitsharma16334 <rohitsharma16334@gmail.com>
2026-02-17 00:09:01 +0530

    Update MultiplyTwoNumbers.java

--- MultiplyTwoNumbers.java
+++ MultiplyTwoNumbers.java
@@ -2,7 +2,7 @@ public class MultiplyTwoNumbers {
 
     public static void main(String[] args) {
 
-        float first = 1.5f;
+        float first = 1.7f;
         float second = 2.0f;
 
         float product = first * second;
